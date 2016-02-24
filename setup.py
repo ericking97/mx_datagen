431963544f2e5691ba19d4cf242bdf39d1ffb460
ericking97 <esanchez@mimoni.com>
2016-02-23 19:39:43 -0600

    Hot fixed problem when someone installed the package

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+
 setup(name='mx_datagenerator',
-      version='0.4.0',
+      version='0.4.3',
       description='Generador de datos específicamente creado para México',
       url='https://github.com/ericking97/MX_datagen.git',
       author='ericking97',
       author_email='ericking97@hotmail.com',
       license='MIT',
       packages=find_packages(),
-      data_files=[('mx_datagen/data_source/', ['mx_datagen/data_source/CP.json'])],
+      install_requires=[
+          'python-dateutil',
+      ],
+      include_package_data=True,
       zip_safe=False)
